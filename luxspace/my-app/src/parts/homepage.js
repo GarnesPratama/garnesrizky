9d6c554d931f6579cc4caddac421073910f1c596
GarnesPratama <97274527+GarnesPratama@users.noreply.github.com>
2022-01-07 13:35:11 +0700

    Update homepage.js

--- luxspace/my-app/src/parts/homepage.js
+++ luxspace/my-app/src/parts/homepage.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "../scene/header";
 import Main from "../scene/main";
+import Footer from "../scene/fooet";
 export default function homepage() {
   return (
     <div>
       <Header />
       <Main />
+      <footer />
     </div>
   );
 }
