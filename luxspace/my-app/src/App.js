53cc3cb5ebb11571055d14d4d221fcf224a33337
GarnesPratama <97274527+GarnesPratama@users.noreply.github.com>
2022-04-11 09:41:27 +0700

    tes

--- luxspace/my-app/src/App.js
+++ luxspace/my-app/src/App.js
@@ -1,8 +1,31 @@
-import "../src/homepage.css";
-import Homepage from "./parts/homepage";
+import React, { Component } from "react";
+import { LinkHTMLAttributes } from "react";
 
-function App() {
-  return <Homepage />;
+class header extends Component {
+  render() {
+    return (
+      <div className="border-b-2 border-blank">
+        <div className="container mx-auto ">
+          <div className=" my-5 ">
+            <nav className="navbar ">
+              <div className="list justify-end flex  ">
+                <ul className=" mx-14 flex  gap-x-6 font-sans -mt-7 font-medium text-base invisible lg:visible  ">
+                  <LinkHTMLAttributes to="">
+                    <li className="hover:text-blue text-blue">Home</li>{" "}
+                  </LinkHTMLAttributes>
+
+                  <li className="hover:text-blue">Browse By</li>
+
+                  <li className="hover:text-blue">Stories</li>
+
+                  <li className="hover:text-blue">Agents</li>
+                </ul>
+              </div>
+            </nav>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
-
-export default App;
+export default header;
